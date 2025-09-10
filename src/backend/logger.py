3ca36f06d3ecd7a83ae36ad039ac20085dc4ae4a
Lunareonn <fuckyourself@no.com>
2025-09-10 07:26:40 +0300

    Change sse logging to SocketIO logging

--- src/backend/logger.py
+++ src/backend/logger.py
@@ -1,14 +1,15 @@
 import logging
 import queue
+from frontend.extensions import socketio
 
 
 log_queue = queue.Queue()
 
-
-class SSELogHandler(logging.Handler):
+class SocketIOHandler(logging.Handler):
     def emit(self, record):
         log_entry = self.format(record)
-        log_queue.put(log_entry)
+        print("emitting:", log_entry)
+        socketio.emit("message", log_entry)
 
 
 spotdl_logger = logging.getLogger("spotdl")
@@ -17,9 +18,10 @@ spotdl_logger.setLevel(logging.INFO)
 downloader_logger = logging.getLogger("downloader")
 downloader_logger.setLevel(logging.INFO)
 
-sse_handler = SSELogHandler()
-sse_handler.setFormatter(logging.Formatter("%(message)s"))
-spotdl_logger.addHandler(sse_handler)
+sio_handler = SocketIOHandler()
+sio_handler.setFormatter(logging.Formatter("%(message)s"))
+spotdl_logger.addHandler(sio_handler)
+downloader_logger.addHandler(sio_handler)
 
 def log(msg):
     downloader_logger.info(msg)
